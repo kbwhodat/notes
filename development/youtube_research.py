27c67b3003ef74f9141e90ca88971ba46fdaff70
kbwhodat <kato.byantalo@aiazing.com>
2023-12-08 14:26:40 -0500

    Added notes for setup-image.nvim.norg

--- development/youtube_research.py
+++ development/youtube_research.py
@@ -8,13 +8,13 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 def get_top_2023_afrobeat_songs(max_results):
     request = youtube.search().list(
-        q='Afrobeats',              # Search query
+        q='uk afroswing songs',              # Search query
         part='snippet',            # Part of the data to be retrieved
         maxResults=max_results,    # Number of results to return
         order='viewCount',         # Order by view count
         type='video',              # Look for videos
         videoCategoryId='10',      # Category ID for Music
-        publishedAfter='2023-10-01T00:00:00Z', # Videos published after this date
+        publishedAfter='2023-01-01T00:00:00Z', # Videos published after this date
         publishedBefore='2023-12-31T23:59:59Z', # Videos published before this date
     )
     response = request.execute()
